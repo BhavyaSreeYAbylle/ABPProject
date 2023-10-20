ca6376a207c6fd283873830668b7c490bb98d528
BhavyaSreeYAbylle <bhavya.yarragonda@abylle.com>
2023-10-20 12:42:27 +0530

    new changes

---
+++ Abp-Angular/aspnet-core/src/Acme.BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Books
+{
+    public class BookDto : AuditedEntityDto<Guid>
+    {
+        public string? Name { get; set; }
+
+        public Book Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
